c1f72dbfb2e6b8824fe133afda3404d17deb6551
= <=>
2024-10-20 23:52:35 -0300

    Configurando arquivo scanner e config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	IgnoreDirs []string          `json:"ignore_dirs"`
+	Patterns   map[string]string `json:"patterns"`
+}
+
+func LoadConfig(filepath string) (Config, error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	var config Config
+	if err := json.NewDecoder(file).Decode(&config); err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module LeakFinder
+
+go 1.23.2
