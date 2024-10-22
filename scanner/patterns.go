eb0afaacb7cbe8184347fd3385e57ac926443c15
= <=>
2024-10-21 23:22:43 -0300

    Realizando scanner e Configurando patterns

--- scanner/patterns.go
+++ scanner/patterns.go
@@ -1,1 +1,32 @@
 package scanner
+
+import "regexp"
+
+var patterns = map[string]string{
+	"AWS Access Key":    `AKIA[0-9A-Z]{16}`,
+	"AWS Secret Key":    `AWS_SECRET_ACCESS_KEY\s*=\s*([A-Za-z0-9/+=]{40})`,
+	"GitHub Token":      `ghp_[0-9a-zA-Z]{36}`,
+	"Bearer Token":      `[A-Za-z0-9-_.]{40}`,
+	"Password Config":   `password\s*=\s*["']?([^\s"']+)["']?`,
+	"MongoDB URI":       `mongodb://[a-zA-Z0-9._%+-]+:[^@]+@[^/]+/[a-zA-Z0-9._%+-]+`,
+	"SQL Connection":    `(Data Source=|Server=)[^;]+;User ID=[^;]+;Password=[^;]+`,
+	"SSH Private Key":   `-----BEGIN (RSA|DSA|ECDSA|ED25519) PRIVATE KEY-----[\\s\\S]+-----END \\1 PRIVATE KEY-----`, // Escapado corretamente
+	"Stripe Secret Key": `sk_test_[0-9a-zA-Z]{24}`,
+	"API Key":           `(?i)(api[-_]?key|apikey|key|secret)`,
+	"Senha":             `(?i)(senha|password|pwd|pass)`,
+	"Token":             `(?i)(token|auth|bearer)`,
+}
+
+func GetPatterns() (map[string]*regexp.Regexp, error) {
+	compiledPatterns := make(map[string]*regexp.Regexp)
+
+	for name, pattern := range patterns {
+		compiled, err := regexp.Compile(pattern)
+		if err != nil {
+			return nil, err
+		}
+		compiledPatterns[name] = compiled
+	}
+
+	return compiledPatterns, nil
+}
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,1 +1,88 @@
 package scanner
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+var CompiledPatterns map[string]*regexp.Regexp
+
+func ReceiveRepo() string {
+	repoPath := flag.String("repo", "", "Caminho para o Repositório")
+	flag.Parse()
+
+	if *repoPath == "" {
+		fmt.Println("[ERRO] Você deve fornecer o caminho para o repositório usando --repo")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Caminho para o repositório: %s\n", *repoPath)
+	return *repoPath
+}
+
+func ReadPath(repoPath string) {
+	fmt.Printf("[INFO] Iniciando a leitura do repositório: %s\n", repoPath)
+	err := filepath.WalkDir(repoPath, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Printf("[ERRO] Não foi possível acessar o caminho: %s\n", path)
+			return err
+		}
+
+		if d.IsDir() {
+			if d.Name() == ".git" {
+				return filepath.SkipDir
+			}
+		} else {
+			if err := ReadFile(path, repoPath); err != nil {
+				fmt.Printf("[ERRO] Falha ao ler o arquivo: %s\n%v\n", path, err)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("[ERRO] Ocorreu um erro durante a varredura do Repositório: %s\n%v\n", repoPath, err)
+	}
+}
+
+func ReadFile(archive string, repoPath string) error {
+	file, err := os.Open(archive)
+	if err != nil {
+		fmt.Printf("[ERRO] Não foi possível abrir o arquivo %s\n", archive)
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	const maxCapacity = 1024 * 1024
+	scanner.Buffer(make([]byte, 0, maxCapacity), maxCapacity)
+	line := 1
+
+	for scanner.Scan() {
+		lineContent := scanner.Text()
+
+		for name, pattern := range CompiledPatterns {
+			if pattern.MatchString(lineContent) {
+				relativePath, err := filepath.Rel(repoPath, archive)
+				if err != nil {
+					fmt.Printf("[ERRO] Não foi possível calcular o caminho relativo: %v\n", err)
+					continue
+				}
+				fmt.Printf("[ATENÇÃO] Vazamento Encontrado em %s: %s\n Linha: %d\n --> %s\n\n", relativePath, name, line, lineContent)
+				break
+			}
+		}
+		line++
+	}
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("[ERRO] Não foi possível ler o arquivo: %s\n%v", archive, err)
+	}
+
+	return nil
+}
